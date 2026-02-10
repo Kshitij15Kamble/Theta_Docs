12cfe024060935d992252154283665045b8c118d
Kshitij <kshitij2201kamble@gmail.com>
2026-02-10 15:40:40 +0530

    Remove jazzmin for deployment

--- core/settings.py
+++ core/settings.py
@@ -36,8 +36,6 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
