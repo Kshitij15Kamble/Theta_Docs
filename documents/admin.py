3cd6240cd05fe9761e509f6adb407dd0c0de0290
Kshitij <kshitij2201kamble@gmail.com>
2026-02-11 11:54:02 +0530

    Remove admin.py auto admin code

--- documents/admin.py
+++ documents/admin.py
@@ -129,11 +129,3 @@ admin.site.site_title = "Theta Docs Admin"
 admin.site.index_title = "Theta Docs Management"
 
 
-
-from django.contrib.auth.models import User
-
-def create_admin():
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
-
-create_admin()
