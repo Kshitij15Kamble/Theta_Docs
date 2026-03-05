820a89b62d3d7200d3a8cb8d767eb42e134f2296
Kshitij <kshitij2201kamble@gmail.com>
2026-03-05 11:08:14 +0530

    Auto create superadmin for Render

--- documents/apps.py
+++ documents/apps.py
@@ -2,5 +2,18 @@ from django.apps import AppConfig
 
 
 class DocumentsConfig(AppConfig):
+   
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'documents'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        username = "Main_admin"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email="admin@example.com",
+                password="Admin@123"
+            )
